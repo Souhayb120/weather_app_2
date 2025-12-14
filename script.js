ec9c77470bb646eaf551f38894c8656c5b968424
souhayb <samihadi120@gmail.com>
2025-12-14 21:05:41 +0100

    new

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const statuss = document.getElementById('status');
 const wind_km = document.getElementById('wind-km');
 const humidty_ps = document.getElementById('humidty-ps');
 const cityCountry = document.getElementById('cityCountry');
-const img = document.getElementById('wheather');
+const img = document.getElementById('ws');
 const searchCity = document.getElementById('search_bar');
 
 
@@ -14,23 +14,41 @@ const cityFound = searchCity.value;
 const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityFound}&appid=36ae07671208095b06a5bf6a19bc2e80&units=metric
 `)
 const data = await responce.json();
-degree.innerText = data.main.temp;
+degree.innerText = data.main.temp + "°C";
 statuss.innerText = data.weather[0].description;
 wind_km.innerText = data.wind.speed + " km / h";
 humidty_ps.innerText = data.main.humidity + "%";
 cityCountry.innerText = data.name + "," + data.sys.country;
 city.innerText = data.name;
 
-switch (data.weather[0].description) {
-    case 'broken clouds':
-      img.src = "./images/animated/cloudy-night-1.svg"
+switch (data.weather[0].main) {
+    case 'Clouds':
+      img.src = "./images/animated/brokenC.svg"
+        break;
+case 'Clear':
+        img.src = "./images/animated/clearSky.svg"
         break;
 
+        case 'Rain':
+        img.src = "./images/animated/showerRain.svg"
+        break;
+      
+            case 'thunderstorm':
+        img.src = "./images/animated/thunder.svg"
+        break;
+               case 'Snow':
+        img.src = "./images/animated/snowy-.svg"
+        break;
     default:
-        img.src = './images/animated/cloudy-night-1.svg'
         break;
 }
 
 }
 
+document.addEventListener("keypress", (event) => {
+  const keyName = event.key;
+  if (keyName === "Enter") {
+    btn.click();
+  }
 
+})
